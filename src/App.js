332338465b5a5d038a27bb51d52c1f1406478c6e
sanofar-glitch <sansano183567@gmail.com>
2025-06-25 20:31:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+       <h1>Hello World</h1>
+      <h2>1st react programk</h2>
     </div>
   );
 }
